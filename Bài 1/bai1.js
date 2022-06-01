e9e93b83da7d54c691d232d85a3b6a9c2a456b51
KangD1804 <18146042@student.hcmus.edu.vn>
2022-06-01 10:26:33 +0700

    enterr

--- Bài 1/bai1.js
+++ Bài 1/bai1.js
@@ -1,8 +1,3 @@
-// let starNum =document.getElementById(start);
-// let endNum =document.getElementById(end);
-// let resEle =document.getElementById(result);
-// let butEle =document.getElementById(enter);
-
 function range(startNum,endNum) {
     const array = [];
     let loop = startNum + 1
@@ -15,35 +10,3 @@ function range(startNum,endNum) {
     return array
 
 };
-
-function onClick() {
-    if (inputsAreEmpty()) {
-      label.textContent = 'Error: one or both inputs are empty.';
-      return;
-    }
-    updateLabel();
-  }
-  function inputsAreEmpty() {
-    if (getNumber1() === '' || getNumber2() === '') {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  function updateLabel() {
-    var startNum = getNumber1();
-    var endNum = getNumber2();
-   
-    console.log(':array',array)
-  }
-  function getNumber1() {
-    return inputs[0].value;
-  }
-  function getNumber2() {
-    return inputs[1].value;
-  }
-  var inputs = document.querySelectorAll('input');
-  var label = document.querySelector('p');
-  var button = document.querySelector('button');
-  button.addEventListener('click', onClick);
-  
